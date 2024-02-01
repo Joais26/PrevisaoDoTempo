d7fce0702ef0ebc15d3d056a293beb13664c5f68
Joais26 <jsverdan26@hotmail.com>
2024-02-01 13:14:33 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,16 @@ function clickOnButton(){
 
 async function searchCity(city){
 
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(response => response.json())
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`).then(response => response.json())
 
    putDataOnScreen(data)
 }
 
 function putDataOnScreen(data){
     console.log(data)
-    document.querySelector(".city").innerHTML = "Temp em " + data.name
-  
+    document.querySelector(".city").innerHTML = "Temp em " + data.name   
+    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C"
+    document.querySelector(".preview-text").innerHTML = data.weather[0].description
+    document.querySelector(".moisture").innerHTML = "Moisture: " + data.main.humidity + " %"
+    document.querySelector(".img-forecast").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
